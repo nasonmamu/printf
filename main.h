5b0f4bc8fa53dd43b93b8359816c3ba727359a1f
nasonmamu <nasonmamu94@gmail.com>
2023-10-18 12:16:44 -0700

    rmoved the space between the characters

--- main.h
+++ main.h
@@ -7,9 +7,9 @@
 typedef struct format
 {
 	char *id;
-	int (*f) ();
-}match;
-int printf_char(va_list val);
+	int (*f);
+}	match;
+
 int _putchar(char c);
 int _printf(const char *format, ...);
 int printf_string(va_list val);
@@ -17,9 +17,9 @@ int _strlen(char *str);
 int _strlenc(const char *str);
 int print_37(void);
 int print_unsigned(va_list types, char buffer[];
-int flags,int width,int precission, int size);
-int print_non_printable(va_list types,char buffer[],
-int flags,int width, int precision, int size;
+int flags, int width, int precission, int size);
+int print _ non _ printable(va_list types, char buffer[],
+int flags, int width, int precision, int size;
 int print_bin(va_list val);
 int print_unsigned(va_list args);
 int print_oct(var_list val);

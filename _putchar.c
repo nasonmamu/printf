7111511a5289daa9d4f77649993a89c2a4a1d323
nasonmamu <nasonmamu94@gmail.com>
2023-11-01 21:52:35 -0700

    contain prototype of c files

---
+++ _putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print printf
+ *
+ * Return: On success 
+ * On error, 1 is returned, and errno is set appropriately.
+*/
+int _putchar(char c)
+{
+	return (0);
+}

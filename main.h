da0113af595044fecb4a7f73e56f3bb4d74699c3
nasonmamu <nasonmamu94@gmail.com>
2023-11-01 21:45:34 -0700

    added putchar prototype

--- main.h
+++ main.h
@@ -112,5 +112,6 @@ unsigned int convert_sbase(buffer_t *output, long int num, char *base,
 		unsigned char flags, int wid, int prec);
 unsigned int convert_ubase(buffer_t *output, unsigned long int num, char *base,
 		unsigned char flags, int wid, int prec);
+int _putchar(char c);
 
 #endif
